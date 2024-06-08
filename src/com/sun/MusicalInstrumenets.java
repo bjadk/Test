efa1b856dbead91867d73b25fd2933b38ea2cebb
bjadk <www.778572846@gmail.com>
2024-06-08 13:22:11 +0900

    update

--- src/com/sun/MusicalInstrumenets.java
+++ src/com/sun/MusicalInstrumenets.java
@@ -1,10 +1,23 @@
 package com.sun;
 
-public class MusicalInstrumenets {
+/**
+ * 「楽器」基底クラスと、その派生クラス「ギター」と「ピアノ」を作成します。基底クラスには抽象メソッドplay()を定義し、
+ * 派生クラスでこのメソッドを実装してください。それぞれの楽器で異なる音を出すようにし、多様性を示す簡単なプログラムを作成します。
+ */
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public abstract class MusicalInstrumenets {
+ String name;
+ String sound;
+ public void setName( String name) {
+	 this.name= name;
+ }
+ public void showName() {
+	 System.out.println("Name: "+name);
+ }
+ public void setSound( String sound) {
+	 this.sound =sound;// mmm
+ }
+	
 	}
 
-}
+
